af6d821a36157d2e7af5cfa5917d906ad64d99ef
huangyan13 <huangyan13@meituan.com>
2018-10-24 20:34:43 +0800

    实现select，insert不忽略，update忽略功能

--- src/main/java/com/nn/mybatis/plugins/IgnoreColumnPlugin.java
+++ src/main/java/com/nn/mybatis/plugins/IgnoreColumnPlugin.java
@@ -1,144 +1,184 @@
 package com.nn.mybatis.plugins;
 
-import java.util.List;
-
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.ShellRunner;
-import org.mybatis.generator.api.dom.java.Field;
-import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
-import org.mybatis.generator.api.dom.java.JavaVisibility;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.Parameter;
-import org.mybatis.generator.api.dom.java.PrimitiveTypeWrapper;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
-import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Element;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.TableConfiguration;
 
+import java.util.*;
+
+/**
+ * @author admin
+ */
 public class IgnoreColumnPlugin extends PluginAdapter {
 
-	@Override
-	public boolean validate(List<String> list) {
+    /**
+     * 要忽略的列
+     */
+    private Map<String, List<String>> ignoreColumns = new HashMap<>();
 
-		return true;
-	}
+    private static final String IGNORE_COLUMNS_FLAG = "ignoreColumns";
 
-	/**
-	 * 为每个Example类添加limit和offset属性已经set、get方法
-	 */
-	@Override
-	public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-		PrimitiveTypeWrapper integerWrapper = FullyQualifiedJavaType.getIntInstance().getPrimitiveTypeWrapper();
-
-		Field limit = new Field();
-		limit.setName("limit");
-		limit.setVisibility(JavaVisibility.PRIVATE);
-		limit.setType(integerWrapper);
-		topLevelClass.addField(limit);
-
-		Method setLimit = new Method();
-		setLimit.setVisibility(JavaVisibility.PUBLIC);
-		setLimit.setName("setLimit");
-		setLimit.addParameter(new Parameter(integerWrapper, "limit"));
-		setLimit.addBodyLine("this.limit = limit;");
-		topLevelClass.addMethod(setLimit);
-
-		Method getLimit = new Method();
-		getLimit.setVisibility(JavaVisibility.PUBLIC);
-		getLimit.setReturnType(integerWrapper);
-		getLimit.setName("getLimit");
-		getLimit.addBodyLine("return limit;");
-		topLevelClass.addMethod(getLimit);
-
-		Field offset = new Field();
-		offset.setName("offset");
-		offset.setVisibility(JavaVisibility.PRIVATE);
-		offset.setType(integerWrapper);
-		topLevelClass.addField(offset);
-
-		Method setOffset = new Method();
-		setOffset.setVisibility(JavaVisibility.PUBLIC);
-		setOffset.setName("setOffset");
-		setOffset.addParameter(new Parameter(integerWrapper, "offset"));
-		setOffset.addBodyLine("this.offset = offset;");
-		topLevelClass.addMethod(setOffset);
-
-		Method getOffset = new Method();
-		getOffset.setVisibility(JavaVisibility.PUBLIC);
-		getOffset.setReturnType(integerWrapper);
-		getOffset.setName("getOffset");
-		getOffset.addBodyLine("return offset;");
-		topLevelClass.addMethod(getOffset);
-
-		return true;
-	}
-
-	/**
-	 * 为Mapper.xml的selectByExample添加limit
-	 */
-	@Override
-	public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
-			IntrospectedTable introspectedTable) {
-
-		XmlElement ifLimitNotNullElement = new XmlElement("if");
-		ifLimitNotNullElement.addAttribute(new Attribute("test", "limit != null"));
-
-		XmlElement ifOffsetNotNullElement = new XmlElement("if");
-		ifOffsetNotNullElement.addAttribute(new Attribute("test", "offset != null"));
-		ifOffsetNotNullElement.addElement(new TextElement("limit ${offset}, ${limit}"));
-		ifLimitNotNullElement.addElement(ifOffsetNotNullElement);
-
-		XmlElement ifOffsetNullElement = new XmlElement("if");
-		ifOffsetNullElement.addAttribute(new Attribute("test", "offset == null"));
-		ifOffsetNullElement.addElement(new TextElement("limit ${limit}"));
-		ifLimitNotNullElement.addElement(ifOffsetNullElement);
-
-		element.addElement(ifLimitNotNullElement);
-
-		return true;
-	}
-
-	@Override
-	public boolean sqlMapUpdateByExampleSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-
-		System.out.println(introspectedTable.getTableConfiguration().getProperties());
-
-		return super.sqlMapUpdateByExampleSelectiveElementGenerated(element, introspectedTable);
-	}
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
 
 
-	@Override
-	public boolean sqlMapUpdateByExampleWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		return super.sqlMapUpdateByExampleWithBLOBsElementGenerated(element, introspectedTable);
-	}
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        // 初始化配置
+        TableConfiguration tableConfiguration = introspectedTable.getTableConfiguration();
+        Properties properties = tableConfiguration.getProperties();
+        for (Map.Entry<Object, Object> entryEntry : properties.entrySet()) {
+            if (entryEntry.getKey().equals(IGNORE_COLUMNS_FLAG)) {
+                List<String> oneTableIgnoreColumns = Arrays.asList(entryEntry.getValue().toString().split(","));
+                ignoreColumns.putIfAbsent(tableConfiguration.getTableName(), oneTableIgnoreColumns);
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public boolean sqlMapUpdateByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		return super.sqlMapUpdateByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
-	}
 
-	@Override
-	public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		return super.sqlMapUpdateByPrimaryKeySelectiveElementGenerated(element, introspectedTable);
-	}
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
+                                                                     IntrospectedTable introspectedTable) {
+        return true;
+    }
 
-	@Override
-	public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		return super.sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(element, introspectedTable);
-	}
+    @Override
+    public boolean sqlMapUpdateByExampleSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByExampleSelectiveElementGenerated(element, introspectedTable);
+    }
 
-	@Override
-	public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-		return super.sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(element, introspectedTable);
-	}
 
-	public static void generate() {
-		String config = IgnoreColumnPlugin.class.getClassLoader().getResource("mybatis-generator.xml").getFile();
-		String[] arg = { "-configfile", config, "-overwrite" };
-		ShellRunner.main(arg);
-	}
-	public static void main(String[] args) {
-		generate();
-	}
+    @Override
+    public boolean sqlMapUpdateByExampleWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByExampleWithBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByPrimaryKeySelectiveElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        ignoreColumnsElement(element.getElements(), introspectedTable);
+        return super.sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(element, introspectedTable);
+    }
+
+
+    /**
+     * 忽略更新的字段
+     */
+    private void ignoreColumnsElement(List<Element> elements, IntrospectedTable introspectedTable) {
+        String tableName = introspectedTable.getTableConfiguration().getTableName();
+        List<String> oneTableIgnore = ignoreColumns.get(tableName);
+
+        List<Element> needIgnore = new ArrayList<>();
+        for (Element ele : elements) {
+            if (ele instanceof TextElement) {
+                if (isTextElementNeedIgnore(ele, oneTableIgnore)) {
+                    needIgnore.add(ele);
+                }
+            }
+            if (ele instanceof XmlElement) {
+                xmlElementFilter(((XmlElement) ele).getElements(), oneTableIgnore);
+            }
+        }
+        elements.removeAll(needIgnore);
+    }
+
+
+    private void xmlElementFilter(List<Element> elements, List<String> oneTableIgnore) {
+        List<Element> needIgnore = new ArrayList<>();
+        for (Element ele : elements) {
+            if (ele instanceof XmlElement) {
+                if (isXmlElementNeedIgnore(ele, oneTableIgnore)) {
+                    needIgnore.add(ele);
+                }
+            }
+        }
+        elements.removeAll(needIgnore);
+    }
+
+    private boolean isXmlElementNeedIgnore(Element ele, List<String> oneTableIgnore) {
+        for (Element oneEle : ((XmlElement) ele).getElements()) {
+            if (oneEle instanceof TextElement) {
+                if (isTextElementNeedIgnore(oneEle, oneTableIgnore)) {
+                    return true;
+                }
+            }
+
+            if (oneEle instanceof XmlElement) {
+                return isXmlElementNeedIgnore(ele, oneTableIgnore);
+            }
+        }
+        return false;
+    }
+
+
+    private boolean isTextElementNeedIgnore(Element ele, List<String> oneTableIgnore) {
+        String text = ((TextElement) ele).getContent().trim();
+        for (String oneIgnore : oneTableIgnore) {
+            System.out.println(text);
+            if (text.matches("^" + oneIgnore + " =.*")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 忽略更新的字段
+     */
+    private void ignoreColumnsFilter(IntrospectedTable introspectedTable) {
+        List<IntrospectedColumn> baseColumns = introspectedTable.getBaseColumns();
+        String tableName = introspectedTable.getTableConfiguration().getTableName();
+        List<String> oneTableIgnore = ignoreColumns.get(tableName);
+
+        List<IntrospectedColumn> needRemovedColumns = new ArrayList<>();
+
+        for (IntrospectedColumn oneColumn : baseColumns) {
+            if (oneTableIgnore.contains(oneColumn.getActualColumnName())) {
+                needRemovedColumns.add(oneColumn);
+            }
+
+        }
+        for (IntrospectedColumn oneCol : needRemovedColumns) {
+            introspectedTable.getBaseColumns().remove(oneCol);
+        }
+    }
+
+    public static void generate() {
+        String config = IgnoreColumnPlugin.class.getClassLoader().getResource("mybatis-generator.xml").getFile();
+        String[] arg = {"-configfile", config, "-overwrite"};
+        ShellRunner.main(arg);
+    }
+
+    public static void main(String[] args) {
+        generate();
+    }
 }
